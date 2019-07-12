1709010585e6bfb704778642906fbfac283d3c08
eulcoco <eulcoco@gmail.com>
2019-07-12 20:18:47 +0200

    1st commit message

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Sebastien Pastor on 7/12/19.
+//  Copyright © 2019 Sebastien Pastor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
